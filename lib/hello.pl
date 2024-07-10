e713314a0fb25f9c6c2e76d292bcf90934e28ec9
Mohsin Raza <mohsin.raza@10pearls.com>
2024-07-10 15:50:10 +0500

    Create hello.pl

---
+++ lib/hello.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl
+# chmod 0755 hello.pl
+
+# Force us to write good code and to provide information on how to correct errors
+use warnings;
+use strict;
+use diagnostics;
+
+# Prints a line followed by a newline
+use feature 'say';
+
+# Use Perl version 5.16
+use v5.16;
+
+# I'm a comment
+# Executes in the terminal when you type 'perl hello.pl'
+print "Hello World\n";
